d2c07616350d83f26fe3995e6fda54f780044e2e
unknown <ispanta03@gmail.com>
2024-04-02 11:32:54 +0545

    recieving data from esp 32

--- esp_server/server.py
+++ esp_server/server.py
@@ -1,13 +1,22 @@
-
 from flask import Flask, request, jsonify
+import datetime
 
 app = Flask(__name__)
 
-app.route('/data', methods=['POST'])
+@app.route('/', methods=['GET'])
+def get_index():
+    return "Hello World"
+
+@app.route('/data', methods=['POST'])
 def handle_data():
     data = request.get_json()
-    print(data)
-    return 200
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    magnitude = data['magnitude']
+    data_line = f"{timestamp}, {magnitude}\n"
+    with open("data.md", "a") as file:
+            file.write(data_line)
+    return str(200)
 
-if __name__ ==  "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+    
